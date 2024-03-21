ce6ed65fcc5e7cc741b89649ae0659e7b302ffa9
corni <gmigan.a@gmail.com>
2024-03-21 15:55:19 +0100

    feat: add responsivenss once the link is copy to the clipboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import { Bounce, ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const url = "https://www.frontendmentor.io/";
 
   const handleClick = async () => {
     await navigator.clipboard.writeText(url);
+    toast.success('Link copied into the clipboard !', {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
   }
+
   return (
     <>
       <main className='bg-primary-light h-screen relative'>
@@ -14,6 +28,7 @@ function App() {
           <h3 className="text-center font-bold text-[22px]">Improve your front-end skills by building projects</h3>
           <p className="text-center font-normal text-gray-500 tracking-[0.19px]">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </article>
+        <ToastContainer />
       </main>
     </>
   );
